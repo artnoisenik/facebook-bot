08068768536650adf762143afd309e23a0ca23f3
Phil Skaggs <prskaggsjr@gmail.com>
2016-06-09 20:59:49 -0600

    added basic weather

--- app.js
+++ app.js
@@ -135,36 +135,8 @@ const firstEntityValue = (entities, entity) => {
 // Our bot actions
 const actions = {
   say(sessionId, context, message, cb) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      fbMessage(recipientId, message, (err, data) => {
-        if (err) {
-          console.log(
-            'Oops! An error occurred while forwarding the response to',
-            recipientId,
-            ':',
-            err
-          );
-        }
-
-        // Let's give the wheel back to our bot
-        cb();
-      });
-    } else {
-      console.log('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      cb();
-    }
-  },
-  merge(sessionId, context, entities, message, cb) {
-    cb(context);
-  },
-  error(sessionId, context, error) {
-    console.log(error.message);
+    console.log(message);
+    cb();
   },
   merge(sessionId, context, entities, message, cb) {
     // Retrieve the location entity and store it into a context field
@@ -183,9 +155,8 @@ const actions = {
     context.forecast = 'sunny';
     cb(context);
   },
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
 };
+
 
 // Setting up our bot
 const wit = new Wit(WIT_TOKEN, actions);
