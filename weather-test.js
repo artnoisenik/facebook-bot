b72b9bbe5de03edfee3754e6d014a020ffd91d86
Phil Skaggs <prskaggsjr@gmail.com>
2016-06-16 17:46:48 -0600

    figured out how to make a get request with es6

---
+++ weather-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+
+const OPEN_WEATHER_TOKEN = process.env.OPEN_WEATHER_TOKEN;
+
+const apiCall = (location) => request({
+  uri: 'http://api.openweathermap.org/data/2.5/weather',
+  method: 'GET',
+  encoding: 'utf-8',
+  json: true,
+  qs: { q: location, appid: OPEN_WEATHER_TOKEN},
+  headers: {'Content-Type': 'application/json'},
+})
+  .on('data', function (data) {
+    console.log('DATATATAT',data);
+  })
+
+apiCall('tokyo');
