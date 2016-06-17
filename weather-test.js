ac211571af2c802820dd4d56f9c99e3353ce8b82
Phil Skaggs <prskaggsjr@gmail.com>
2016-06-17 10:40:21 -0600

    akyuna fixed stuff

--- weather-test.js
+++ weather-test.js
@@ -5,21 +5,40 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
+const axios = require('axios');
 
 const OPEN_WEATHER_TOKEN = process.env.OPEN_WEATHER_TOKEN;
 
 // let info;
-const apiCall = (location) => request({
-  uri: 'http://api.openweathermap.org/data/2.5/weather',
-  method: 'GET',
-  encoding: 'utf-8',
-  json: true,
-  qs: { q: location, appid: OPEN_WEATHER_TOKEN},
-  headers: {'Content-Type': 'application/json'},
-})
-  .on('data', function (data) {
-    let stuff = JSON.parse(data);
-    console.log(stuff);
-  })
 
-apiCall('tokyo');
+const apiCall = (location) => {
+return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${OPEN_WEATHER_TOKEN}`).then((res) => {
+  // console.log('res from axios', res.data);
+  return res.data;
+})
+//   request({
+//   uri: 'http://api.openweathermap.org/data/2.5/weather',
+//   method: 'GET',
+//   encoding: 'utf-8',
+//   json: true,
+//   qs: { q: location, appid: OPEN_WEATHER_TOKEN},
+//   headers: {'Content-Type': 'application/json'},
+// })
+// .then((res) => {
+//   console.log('res!', res);
+// })
+// .on('data', function (data) {
+//     let stuff = JSON.parse(data);
+//     console.log('happened', data);
+//     body = stuff;
+//     return stuff;
+//   }).then((res) => {
+//     console.log('res in then after on', res);
+//   })
+
+}
+
+apiCall('tokyo').then((result) => {
+
+  console.log(result);
+});
